18c4d05f8d2b14fe3c94fb00106fb43ee2011647
suhyun <lee171400@gmail.com>
2025-12-12 19:48:59 +0900

    Create StatDelta.java

---
+++ while_true/src/game/stage/morning/StatDelta.java
@@ -0,0 +1,12 @@
+package morning;
+
+public class StatDelta {
+    public final int hp, ment, intel, social;
+
+    public StatDelta(int hp, int ment, int intel, int social) {
+        this.hp = hp;
+        this.ment = ment;
+        this.intel = intel;
+        this.social = social;
+    }
+}
